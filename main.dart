2993755795d66de5a7ebd1e8d407033d8570d107
demormax <sakirbaz@gmail.com>
2025-05-23 19:50:07 +0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,15 +1,38 @@
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(BistSuperScanner());
+void main() => runApp(BISTSuperScannerApp());
 
-class BistSuperScanner extends StatelessWidget {
+class BISTSuperScannerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'BIST SuperScanner',
-      home: Scaffold(
-        appBar: AppBar(title: Text('BIST SuperScanner')),
-        body: Center(child: Text('Merhaba BIST!')),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: StockScannerHome(),
+    );
+  }
+}
+
+class StockScannerHome extends StatelessWidget {
+  final List<String> favoriteStocks = ['ALKA', 'KUTPO', 'ISDMR', 'TMSN'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('BIST SuperScanner')),
+      body: ListView.builder(
+        itemCount: favoriteStocks.length,
+        itemBuilder: (context, index) {
+          final stock = favoriteStocks[index];
+          return Card(
+            child: ListTile(
+              title: Text(stock),
+              subtitle: Text('Statik veri: %2.5 artış sinyali'),
+              trailing: Icon(Icons.trending_up, color: Colors.green),
+            ),
+          );
+        },
       ),
     );
   }
